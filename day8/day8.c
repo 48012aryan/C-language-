3e8374e939abd7732391224b5a95e4c7f16ea54d
48012aryan <134853387+48012aryan@users.noreply.github.com>
2023-06-05 18:23:27 +0530

    Add files via upload

---
+++ day8/day8.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+
+  int main(){
+  	
+  	char pavan,vasu;
+  	
+  	printf("pavan marks: ");
+  	scanf("%d",&pavan);
+  	
+  	printf("vasu marks: ");
+    scanf("%d",&vasu);
+	
+	if(pavan>vasu){
+	    printf ("pavan marks smaller than vasu");
+	 }
+   else{
+   
+	   printf("vasu marks bigger than pavan");	
+	
+		
+	}  
+  	
+  
+  	
+  	
+  	
+  	return 0;
+  }
