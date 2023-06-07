618e141b50a2fb0533fd3b37f672c5cf822f155f
48012aryan <134853387+48012aryan@users.noreply.github.com>
2023-06-07 19:00:19 +0530

    Add files via upload

---
+++ day11/day11menu.c
@@ -0,0 +1,51 @@
+#include<stdio.h>
+
+int main(){
+	
+	int a,b;
+	
+	printf("1 veg food\n");
+	printf("2 non-veg food");
+	
+	printf("select your food :");
+	scanf("%d",&a);
+	
+	
+	switch(a){
+		case 1:
+			printf("\n\n 1. paw bhaji 140Rs\n");
+			printf("2. dhosa 130Rs\n\n");
+		
+	printf("enter your food num :");
+	scanf("%d",&a);
+}
+	switch(a){
+		case 1:
+			printf("butter\n");
+			
+		
+			printf("oil\n");
+	printf("enter your iteam :");
+	scanf("%d",&a);	    
+	
+	}	
+    
+		case 1:
+			printf("\n 1. butter chicken 259Rs\n");
+			printf("2. rogan josh 764Rs");
+			
+		printf("enter your food num :");
+		scanf("%d",&b);
+	}
+	switch(b){
+		case 1:
+			printf("with small chicken pices");
+			break;
+		case 2:
+			printf("with large");
+	
+	
+	}
+	
+	return 0;
+}
