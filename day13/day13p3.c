ffee7a131e6e5d2803dfc5baffed31a979ffb750
48012aryan <134853387+48012aryan@users.noreply.github.com>
2023-06-09 18:17:00 +0530

    Add files via upload

---
+++ day13/day13p3.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+
+int main(){
+	int i = 1;
+	int n;
+	
+	printf("enter your value :");
+	scanf("%d",&n);
+	
+	
+	do{
+		
+		printf("%d\n",i);
+		
+		i++;
+	}	while(i<=n);
+
+	
+
+	
+	
+	return 0;
+}
---
+++ day13/day13p4.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+
+int main(){
+	int i ;
+	int n =1;
+	printf("enter your value :");
+    scanf("%d",&i);
+			
+	
+	
+	do{
+		
+		printf("%d\n",i);
+			i--;
+		
+	}	while(n<=i);
+
+
+
+	
+	
+	return 0;
+}
